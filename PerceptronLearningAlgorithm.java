de075d7b99ad292f7e13c0ac5c239e3372ae17c3
ashtonmendes <ashton.mendes@gmail.com>
2013-10-10 11:14:56 +0530

    Update PerceptronLearningAlgorithm.java
    final algorithm, working correctly

--- PerceptronLearningAlgorithm.java
+++ PerceptronLearningAlgorithm.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-class DataPoint
+class DataPoint 
 {
 	double x0;
 	double x1;
-	double x2;	
+	double x2;
 	int result;
-	
-	public DataPoint(double x1, double x2, int result) 
+
+	public DataPoint(double x1, double x2, int result)
 	{
 		super();
 		this.x0 = 1;
@@ -17,11 +17,12 @@ class DataPoint
 		this.result = result;
 	}
 }
-class Line
+
+class Line 
 {
 	double m;
 	double c;
-	
+
 	public Line(double m, double c)
 	{
 		this.m = m;
@@ -32,175 +33,170 @@ class Line
 public class PerceptronLearningAlgorithm
 {
 	static Line targetFunction;
-	public static DataPoint[] generateTrainingSet(int N)
+	static Random randomNo = new Random();
+
+	public static void generateTargetFunction()
 	{
 		//choose any random line as the target function
-		double X1 = Math.random()*assignRandomSign();
-		double Y1 = Math.random()*assignRandomSign();
-		double X2 = Math.random()*assignRandomSign();
-		double Y2 = Math.random()*assignRandomSign();
-		
+		double X1 = randomNo.nextDouble()*assignRandomSign();
+		double Y1 = randomNo.nextDouble()*assignRandomSign();
+		double X2 = randomNo.nextDouble()*assignRandomSign();
+		double Y2 = randomNo.nextDouble()*assignRandomSign();
+				
 		double m = (Y2-Y1)/(X2-X1);
 		double c = Y1 - m*X1;
-		
+				
 		targetFunction = new Line(m,c);
-		
+				
 		System.out.println("Target Function: y = "+targetFunction.m+"*x + "+targetFunction.c);
-		
+	}
+	
+	public static DataPoint[] generateTrainingSet(int N)
+	{	
 		DataPoint trainingSet[] = new DataPoint[N];
 		
 		for(int i=0; i<N; i++)
 		{
-			double x = Math.random()*assignRandomSign();
-			double y = Math.random()*assignRandomSign();
+			double x = randomNo.nextDouble()*assignRandomSign();
+			double y = randomNo.nextDouble()*assignRandomSign();
 			
 			trainingSet[i] = new DataPoint(x, y, classifyPoint(x, y, targetFunction));
 		}
-		
 		return trainingSet;
 	}
-	
+		
 	public static int classifyPoint(double x, double y, Line targetFunction)
 	{
-		double temp = (y-targetFunction.c)/targetFunction.m;
-		if(x>temp)
-			return 1;
-		if(x<temp)
-			return -1;
-		if(x==temp)
-			return assignRandomSign();
-		return 0;
+			double temp = (y - targetFunction.c) / targetFunction.m;
+			if (x > temp)
+				return 1;
+			else
+				return -1;				
 	}
-	
+		
 	public static int assignRandomSign()
 	{
-		switch(new Random().nextInt(2))
-		{		
-			case 1:
+			switch (randomNo.nextInt(2))
 			{
-				return -1;				
+				case 1: 
+				{
+					return -1;
+				}
+				case 0:
+				default: 
+				{
+					return 1;
+				}
 			}
-			case 0:
-			default:
-			{
-				return 1;
-			}
-		}
-	}
+	}		
 	
-	public static void main(String[] args) {	
-		
-		int N = 100;
-		int loopLimit = 1000;
+	public static void main(String[] args) 
+	{ 
+		int N = 10;
+		int loopLimit = 100;
 		DataPoint trainingSet[];
 		double[] weight;
 		ArrayList<DataPoint> misclassified;
-		Random randomNo = new Random();
-		double condition=0;
-		int classifiedWrongly = 0;
+	    double condition=0;
+		double classifiedWrongly = 0.0;
 		double iterations = 0;
-		double disagreement = 0;		
+		double disagreement = 0; 
 		int loop = 0;
 		
-		while(loop<=loopLimit)
+		while(loop<loopLimit)
 		{
-		loop++;
-		
-		System.out.println(loop);
+			loop++;
+				
+			generateTargetFunction();
+			trainingSet = generateTrainingSet(N);
 			
-		trainingSet = generateTrainingSet(N);
-		
-		System.out.println("******************\nTraining Set Points:");
-		for(DataPoint point: trainingSet)
+			System.out.println("******************\nTraining Set Points:");
+			for(DataPoint point: trainingSet)
 			System.out.println(point.x1+","+point.x2);
-		System.out.println("******************");	
-        		
-		weight = new double[3];
-		weight[0] = 0;
-		weight[1] = 0;
-		weight[2] = 0;
-		
-		misclassified = new ArrayList<DataPoint>();
-		
-		for (DataPoint point : trainingSet) {
-			misclassified.add(point);
-		}	
-		
-		//PLA	
-		condition = 0;
-		while(!misclassified.isEmpty())
-		{		
-			iterations++;
-			
-			int pick = randomNo.nextInt(misclassified.size());			
-			
-			DataPoint point = misclassified.get(pick);		
-			
-			condition = weight[0]*point.x0 + weight[1]*point.x1 + weight[2]*point.x2;
-			
-			if(condition > 0 && point.result == -1)
+			System.out.println("******************"); 
+			        
+			weight = new double[3];
+			weight[0] = 0;
+			weight[1] = 0;
+			weight[2] = 0;
+			 
+			misclassified = new ArrayList<DataPoint>();
+			for (DataPoint point : trainingSet)
 			{
-				weight[0] -= point.x0;
-				weight[1] -= point.x1;
-				weight[2] -= point.x2;
-			}
-			else if(condition < 0 && point.result == 1)
-			{
-				weight[0] += point.x0;
-				weight[1] += point.x1;
-				weight[2] += point.x2;
-			}	
-			else if(condition == 0)
-			{
-				switch(new Random().nextInt(2))
+				misclassified.add(point);
+			} 
+			 
+			//PLA 
+			condition = 0;			
+			while(!misclassified.isEmpty())
+			{ 
+				iterations++;
+							
+				int pick = randomNo.nextInt(misclassified.size()); 
+				
+				DataPoint point = misclassified.get(pick); 
+				
+				condition = weight[0]*point.x0 + weight[1]*point.x1 + weight[2]*point.x2;
+				
+				int calculatedResult = 0;
+				
+				if(condition > 0)
+					calculatedResult = 1;
+				else
+					calculatedResult = -1;
+				 
+				if(calculatedResult != point.result)
 				{
-					case 0:
+					weight[0] +=  (point.result * point.x0);
+					weight[1] +=  (point.result * point.x1);
+					weight[2] +=  (point.result * point.x2);
+				}				
+				
+				misclassified.removeAll(misclassified); 
+				
+				for (int i = 0; i < trainingSet.length; i++)
+				{ 
+					condition = weight[0]*trainingSet[i].x0 + weight[1]*trainingSet[i].x1 +	weight[2]*trainingSet[i].x2;
+									
+					if(condition > 0)
+						calculatedResult = 1;
+					else
+						calculatedResult = -1;
+					 
+					if(calculatedResult != trainingSet[i].result)
 					{
-						weight[0] -= point.x0;
-						weight[1] -= point.x1;
-						weight[2] -= point.x2;
-						break;
-					}
-					case 1:
-					{
-						weight[0] += point.x0;
-						weight[1] += point.x1;
-						weight[2] += point.x2;
-						break;
-					}
+						misclassified.add(trainingSet[i]);
+					}					
 				}
 			}
-				
-			misclassified.removeAll(misclassified);					
 			
-			for (int i = 0; i < trainingSet.length; i++)
-			{	
-				condition = weight[0]*trainingSet[i].x0 + weight[1]*trainingSet[i].x1 + weight[2]*trainingSet[i].x2;
-				
-				if( (condition > 0 && trainingSet[i].result == -1) ||
-					(condition < 0 && trainingSet[i].result == 1) ||
-					(condition == 0) )
-				{
-					misclassified.add(trainingSet[i]);
-				}
-			}
-		}
-				
-		System.out.println("y = "+ weight[1]/weight[2] + "* x  +  "+ weight[0]/weight[2]);
-		
-		Line finalHypothesis = new Line(weight[1]/weight[2], weight[0]/weight[2]);		 
-		for(int i=0; i<trainingSet.length; i++)
-		{
-			if(classifyPoint(trainingSet[i].x1, trainingSet[i].x2, targetFunction) != classifyPoint(trainingSet[i].x1, trainingSet[i].x2, finalHypothesis))
+			System.out.println("Final Hypothesis: y = "+ (-1*weight[1])/weight[2] + "* x  +  "+ (-1*weight[0])/weight[2]);
+					 
+			 //Test on 1000 random points
+			DataPoint[] test = new DataPoint[1000]; 
+			
+			test = generateTrainingSet(1000);			
+
+			double count = 0.0;
+			
+			for(int k1 = 0; k1 < test.length; k1++)
 			{
-				classifiedWrongly++;
-			}
+				double dotProd = (weight[0] + (weight[1] * test[k1].x1) + (weight[2] * test[k1].x2));
+				double calc = 0.0;
+				if(dotProd>0)
+					calc = 1.0;
+				else
+					calc = -1.0;
+				if(test[k1].result != calc)
+					count++; 
+			 }
+			
+			classifiedWrongly += (count/1000.0);		
+			
+		 } 
+		 
+		System.out.println("Avg. No. Of Iterations : "+	iterations/(double)loopLimit);
+		disagreement = (double)classifiedWrongly/(double)(loopLimit);
+		System.out.println("Avg. Probability of disagreement : ("+classifiedWrongly+"/"+(loopLimit)+") = "+disagreement);
 		}
-		
-		}		
-		
-		System.out.println("Avg. No. Of Iterations : "+ iterations/(double)loopLimit);
-		disagreement = (double)classifiedWrongly/(double)(N*loopLimit);
-		System.out.println("Avg. Probability of disagreement : ("+classifiedWrongly+"/"+(N*loopLimit)+") = "+disagreement);
-	}
 }
